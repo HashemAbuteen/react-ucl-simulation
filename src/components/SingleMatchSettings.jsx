993f8f02188ee4944029afd93b72bb2d327932aa
Hashem <hashemabualteen@gmail.com>
2023-03-12 07:00:33 +0200

    adds logging

--- src/components/SingleMatchSettings.jsx
+++ src/components/SingleMatchSettings.jsx
@@ -14,36 +14,61 @@ function SingleMatchSettings({ setPage }) {
   const [homeTeam, setHomeTeam] = useState();
   const [awayTeam, setAwayTeam] = useState();
   const [error, setError] = useState();
+
   useEffect(() => {
     const defaultHome = 529;
+    console.log(`Fetching home team data for ID ${defaultHome}...`);
     if (isTeamInStorage(defaultHome)) {
+      console.log(`Home team data found in storage for ID ${defaultHome}`);
       setHomeTeam(getTeamFromStorage(defaultHome));
     } else {
+      console.log(
+        `Home team data not found in storage for ID ${defaultHome}. Fetching from API...`
+      );
       getTeamDataOnly(defaultHome);
       setTimeout(() => {
         if (isTeamInStorageWithoutStats(defaultHome)) {
+          console.log(
+            `Home team data fetched successfully for ID ${defaultHome}`
+          );
           setHomeTeam(getTeamFromStorage(defaultHome));
         } else {
-          setError("An error occured while loading the home team data");
+          console.error(
+            `An error occurred while loading the home team data for ID ${defaultHome}`
+          );
+          setError("An error occurred while loading the home team data");
         }
       }, 3000);
     }
+
     const defaultAway = 541;
+    console.log(`Fetching away team data for ID ${defaultAway}...`);
     if (isTeamInStorage(defaultAway)) {
+      console.log(`Away team data found in storage for ID ${defaultAway}`);
       setAwayTeam(getTeamFromStorage(defaultAway));
     } else {
+      console.log(
+        `Away team data not found in storage for ID ${defaultAway}. Fetching from API...`
+      );
       getTeamDataOnly(defaultAway);
       setTimeout(() => {
         if (isTeamInStorageWithoutStats(defaultAway)) {
+          console.log(
+            `Away team data fetched successfully for ID ${defaultAway}`
+          );
           setAwayTeam(getTeamFromStorage(defaultAway));
         } else {
-          setError("An error occured while loading the away team data");
+          console.error(
+            `An error occurred while loading the away team data for ID ${defaultAway}`
+          );
+          setError("An error occurred while loading the away team data");
         }
       }, 3000);
     }
   }, []);
 
   if (!homeTeam || !awayTeam) {
+    console.log("One or both teams are still loading...");
     return (
       <div className="single-match-settings">
         <div className="match-container">
@@ -52,6 +77,10 @@ function SingleMatchSettings({ setPage }) {
       </div>
     );
   }
+
+  console.log(
+    `Both teams loaded successfully: ${homeTeam.name} vs ${awayTeam.name}`
+  );
   return (
     <div className="single-match-settings">
       <div className="single-match-teams">
